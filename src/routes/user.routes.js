1efabba0e163d249d74acc5e88c81393cc0b51db
nav-github3 <navneetlonare77@gmail.com>
2025-03-21 06:03:13 +0530

    git add

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { registerUser } from "../controllers/user.controllers.js";
 import {upload} from "../middlewares/multer.middleware.js"
 // import { verifyJWT } from "../middlewares/auth.middleware.js";
 
